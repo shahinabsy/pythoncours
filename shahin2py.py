ccadaa1403d07da71892abdb7ce3bf8696dac74b
shahinabsy <100094320+shahinabsy@users.noreply.github.com>
2022-02-28 00:34:47 +0330

    Create shahin2py.py

---
+++ shahin2py.py
@@ -0,0 +1,19 @@
+x=2
+print("x")
+print('x')
+print(x)
+print("x + 1")
+print('x'+1)
+print(x+1)
+
+
+
+"""
+result
+x
+x
+2
+x + 1
+    print('x'+1)
+TypeError: can only concatenate str (not "int") to str
+"""
