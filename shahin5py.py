db74a2febe7696317426e8ebd690bc991ac40dff
shahinabsy <100094320+shahinabsy@users.noreply.github.com>
2022-03-07 00:32:21 +0330

    Create shahin5py.py

---
+++ shahin5py.py
@@ -0,0 +1,18 @@
+"""
+1 exercise result
+A)i=5 ,j=5 ,k=7
+B)i=3 ,j=5 ,k=5
+C)i=7 ,j=3 ,k=7
+D)i=5 ,j=3 ,k=3
+E)i=5 ,j=3 ,k=5
+F)i=7 ,j=7 ,k=3
+"""
+
+"""
+2 exercises result
+A) 3=>wow3
+B) 21=>who a 21
+C) 5=>6
+D) 17=>27
+E) -5=>wow-5
+"""
